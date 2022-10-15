3b0973229ce9b3d545bf04913a54b2fdfc5bebb7
hazem237 <201082@ppu.edu.ps>
2022-10-15 08:56:10 +0300

    finished app interface

--- src/componets/Grids.jsx
+++ src/componets/Grids.jsx
@@ -2,10 +2,27 @@ import { useState } from "react"
 
 
 const Grids = () => {
+
+    const propriety = {
+        above : [
+            [0,1,2] ,
+            [3,4,5] ,
+            [6,7,8]
+        ] ,
+        down : [ 
+            [0 , 3,6] ,
+            [1,4,7] ,
+            [2,5,8]
+        ] ,
+        across : [ 
+            [0,4,8] ,
+            [2,4,6]
+        ]
+    }
      const [grid , setGrid]=useState(['','','','','','','','',''])
   const [turn ,setTurn]=useState('X')
   const printTurn = (index) => {
-
+    console.log(propriety);
     if( !grid[index].length>0) 
     {
         grid[index]=turn;
@@ -19,7 +36,7 @@ const Grids = () => {
   return (
     <>
       {grid.map((s ,index)=> (
-        <div className= "grid" onClick={()=> printTurn(index)} key={index}>{s}</div>
+        <div className= {` ${s==='' ? `grid` : s==='X' ? `grid  y` : `grid x`} `} onClick={()=> printTurn(index)} key={index}>{s}</div>
       ))}
     </>
   )
